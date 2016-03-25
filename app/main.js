7cee69e0e14090821f90a0a225399f7839c12846
LynnGe <nzgezilin@gmail.com>
2016-03-25 23:15:16 +1300

    Bootstrap-treeview setup   copy bootstrap-treeview.js in treeview.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from './table';
+import Tree from './treeview';
+
 
 ReactDOM.render(
-  <Table />,
+  <div>
+  <Tree />
+  <Table />
+  </div>,
   document.getElementById("app")
 );
 
